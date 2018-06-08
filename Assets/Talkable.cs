9d38154adcd7d5ca6b38f5522f4cd1fd3c9ede02
jiaying <s1050422@gm.pu.eddu.tw>
2018-06-08 23:02:47 +0800

    add look controller

--- Assets/Talkable.cs
+++ Assets/Talkable.cs
@@ -1,9 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using Fungus;//記得引用Fungus
 
 public class Talkable : MonoBehaviour
 {
+    private HeadLookController headController;
+    private GameObject FPSController;
+
+    void Start()
+    {
+        headController = GetComponent<HeadLookController>();
+        FPSController = GameObject.Find("FPSController");
+    }
+
+    void Update()
+    {
+        headController.target = FPSController.transform.position + new Vector3(0,0,1.5f);
+    }
+
     void OnTriggerEnter()
     {
         Flowchart.BroadcastFungusMessage("對話1");
